c4fc17c97d3b6d26a54db0942171e93f8f39b4da
Yawen <sytta@live.ca>
2018-12-17 06:21:14 -0500

    add advancement

---
+++ Project2/baseline_helpers.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+
+def user_standardize(df):
+    mean_per_user = df.groupby('User').mean().Rating
+    var_per_user = df.groupby('User').var().Rating
+    stand_df = df.copy()
+    stand_df['Rating'] = df.apply(lambda x: (x['Rating'] - mean_per_user[x['User']]) / var_per_user[x['User']], axis=1)
+    return stand_df
+
+def user_standardize_recover(df, stand_pred_test):
+    mean_per_user = df.groupby('User').mean().Rating
+    var_per_user = df.groupby('User').var().Rating
+    pred_test = stand_pred_test.copy()
+    pred_test['Rating'] = stand_pred_test.apply(lambda x: (x['Rating'] * var_per_user[x['User']] + mean_per_user[x['User']]), axis=1)
+    return pred_test
+
+def user_habit(df):
+    global_mean = df.Rating.mean()
+    mean_per_user = df.groupby('User').mean().Rating
+    habit =mean_per_user - global_mean
+    return habit
+
+def user_habit_standardize(df):
+    habit = user_habit(df)
+    stand_df = df.copy()
+    stand_df['Rating'] = df.apply(lambda x: x['Rating'] - habit[x['User']], axis=1)
+    return stand_df
+
+def user_habit_standardize_recover(df, stand_pred_test):
+    habit = user_habit(df)
+    pred_test = stand_pred_test.copy()
+    pred_test['Rating'] = stand_pred_test.apply(lambda x: x['Rating'] + habit[x['User']], axis=1)
+    return pred_test
