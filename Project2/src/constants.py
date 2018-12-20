b50503d5be4c009e12ea64db4a6002a77afcf860
Yawen <sytta@live.ca>
2018-12-20 13:04:58 -0500

    change paths in constants

--- Project2/src/constants.py
+++ Project2/src/constants.py
@@ -1,8 +1,8 @@
-train_dataset = "./datas/data_train.csv"
-test_dataset = "./datas/sampleSubmission.csv"
+train_dataset = "../datas/data_train.csv"
+test_dataset = "../datas/sampleSubmission.csv"
 
 surprise_train_path = "surprise_train.csv"
 surprise_test_path = "surprise_test.csv"
 
-folder = "./predict_save/"
-folder_predict = "./train_predictions/"
+folder = "../train_predictions/"
+folder_predict = "../test_predictions/"
